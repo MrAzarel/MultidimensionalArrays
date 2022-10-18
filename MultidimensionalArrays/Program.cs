b1158ae88ff88fa59aa8cb6353137dd67742eca9
MrAzarel <arseniy.mazurenko.03@gmail.com>
2022-10-18 14:17:35 +0300

    Добавьте файлы проекта.

---
+++ MultidimensionalArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Array
+{
+    Random rnd = new Random();
+    public void arrayGenerate(int[] arr, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = rnd.Next();
+        }
+    }
+
+    public void arrayConcludion(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Array array = new Array();
+        int[] arr = new int[5];
+        array.arrayGenerate(arr, 5);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }*/
+    }
+}
